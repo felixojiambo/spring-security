759e8a02a05e05d359419dfaefb115d1a93e2ae6
Felix Ojiambo <felixojiamboe@gmail.com>
2024-03-10 01:51:17 -0800

    AuthRequest

--- src/main/java/com/zep/springsecurity/entity/AuthRequest.java
+++ src/main/java/com/zep/springsecurity/entity/AuthRequest.java
@@ -1,4 +1,15 @@
 package com.zep.springsecurity.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class AuthRequest {
+   private String username;
+   private  String password;
+
 }
